0686d40af35c92da4c38df32ddf03ab25c6c0488
Jacob Solomon <jacob@chonkin.attlocal.net>
2016-05-31 23:35:33 -0400

    bug fixes

--- pixel_density_qualtrics.js
+++ pixel_density_qualtrics.js
@@ -2,42 +2,31 @@ Qualtrics.SurveyEngine.addOnload(function()
 {
 	/*Place Your Javascript Below This Line*/
 
-	var slider = document.getElementById("scale_slider");
-	var credit_card_img = document.getElementById("credit_card");
-	var dollar_img = document.getElementById("dollar");
-	var ruler_img = document.getElementById("ruler");
-	var start_height = credit_card_img.height;
-	var start_width = credit_card_img.width;
+	var jq = jQuery.noConflict();
+	var slider = jq("#scale_slider");
 
-	var dims = {
-		"credit_card" : {
-			"width" : credit_card_img.width,
-			"height" : credit_card_img.height
-		},
-		"dollar" : {
-			"width": dollar_img.width,
-			"height": dollar_img.height
-		},
-		"ruler": {
-			"width": ruler_img.width,
-			"height": ruler_img.height
+
+	var dims = {}
+	jq(".object_image").each(function(){
+		dims[this.id] = {
+			'height': this.height,
+			'width': this.width
 		}
-	}
+	});
 
-	slider.oninput = function(){
-		var scalar = slider.value / 50;
-		for (img in [credit_card_img, dollar_img, ruler_img]){
-			img.height = dims[img.id]['height'] * scalar;
-			img.width = dims[img.id]['width'] * scalar;
-		}
-	}
 
-	slider.onchange = function() {
-		var dpi = credit_card_img.width / 3.37;
 
-		// Or if you are only using a different image:
-		// var dpi = dollar_img.width / 6.14;
-		// var dpi = ruler_img.width / 3;
+	slider.on('input', function(){
+		var scalar = slider.val() / 50;
+		jq(".object_image").each(function(){
+			jq("#"+this.id).height(dims[this.id]['height']*scalar);
+			jq("#"+this.id).width(dims[this.id]['width']*scalar);
+		});
+
+	});
+
+	slider.on('change', function() {
+		var dpi = jq("#credit_card").width / 3.37;
 		this.setEmbeddedData("dpi", dpi);
-	}
+	});
 });
